66bc76dc3201d650d5adfc3ca573d4c90c65d580
Mustafa <mustafakosker@gmail.com>
2017-10-31 23:40:59 +0000

    Update MainReporter.java

--- src/main/java/org/serkan/task/MainReporter.java
+++ src/main/java/org/serkan/task/MainReporter.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 
 public class MainReporter {
 	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		InputParser inputParser = new InputParser(args);
+		if(args==null || args.length==0){
+			throw new IllegalArgumentException("Err: Please add a file name parameter.");
+		}
+		final List<String> lines  = Files.readAllLines(Paths.get(args[0]));
+		//assert lines size here
+		final InputParser inputParser = new InputParser(lines);
+		
 		inputParser.getLogLines().stream().forEach(System.out::println); 
 	}
 }
